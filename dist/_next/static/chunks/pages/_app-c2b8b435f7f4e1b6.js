39b13d0f096abd92150b14afdbfb19e86bf2652f
Rcreators <rishi@rcreators.com>
2025-05-28 15:34:47 +0530

    Dist Removed

--- dist/_next/static/chunks/pages/_app-c2b8b435f7f4e1b6.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{6840:function(n,r,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return e(3847)}])},3847:function(n,r,e){"use strict";e.r(r);var t=e(5893);e(3154),e(5018),r.default=function(n){let{Component:r,pageProps:e}=n;return(0,t.jsx)(t.Fragment,{children:(0,t.jsx)(r,{...e})})}},3154:function(){},5018:function(){},5251:function(n,r,e){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */ var t=e(7294),o=Symbol.for("react.element"),u=Symbol.for("react.fragment"),f=Object.prototype.hasOwnProperty,_=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,c={key:!0,ref:!0,__self:!0,__source:!0};function i(n,r,e){var t,u={},i=null,s=null;for(t in void 0!==e&&(i=""+e),void 0!==r.key&&(i=""+r.key),void 0!==r.ref&&(s=r.ref),r)f.call(r,t)&&!c.hasOwnProperty(t)&&(u[t]=r[t]);if(n&&n.defaultProps)for(t in r=n.defaultProps)void 0===u[t]&&(u[t]=r[t]);return{$$typeof:o,type:n,key:i,ref:s,props:u,_owner:_.current}}r.Fragment=u,r.jsx=i,r.jsxs=i},5893:function(n,r,e){"use strict";n.exports=e(5251)}},function(n){var r=function(r){return n(n.s=r)};n.O(0,[179],function(){return r(6840),r(880)}),_N_E=n.O()}]);
